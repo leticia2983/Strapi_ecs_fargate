4b2b4e6d7710b23e8f999dad463620816ab6c27f
NevilleBAC <neville_bac@outlook.com>
2024-07-02 15:14:46 +0530

    added aws region in aws.yml

--- terraform/main.tf
+++ terraform/main.tf
@@ -101,7 +101,7 @@ resource "aws_ecs_task_definition" "strapi" {
 
 resource "aws_ecs_service" "strapi" {
   name            = "strapi-service"
-  cluster         = aws_ecs_cluster.let-vpc.id
+  cluster         = aws_ecs_cluster.main.id
   task_definition = aws_ecs_task_definition.strapi.arn
   desired_count   = 1
   launch_type     = "FARGATE"
