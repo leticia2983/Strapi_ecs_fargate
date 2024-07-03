587a64c73fb761d54e4c2802a07c9d736285d83b
NevilleBAC <neville_bac@outlook.com>
2024-07-03 10:08:46 +0530

    added detail in provider

--- terraform/provider.tf
+++ terraform/provider.tf
@@ -10,7 +10,7 @@
 provider "aws" {
   region = var.region
   assume_role {
-  task role_arn = "arn:aws:iam::533266978173:role/aws-service-role/ecs.amazonaws.com/AWSServiceRoleForECS"
+  role_arn = "arn:aws:iam::533266978173:role/aws-service-role/ecs.amazonaws.com/AWSServiceRoleForECS"
   execution_role_arn = "arn:aws:iam::533266978173:role/aws-service-role/ecs.amazonaws.com/AWSServiceRoleForECS"
   }
 }
