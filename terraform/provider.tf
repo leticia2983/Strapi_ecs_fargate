5e94c299fdaf75c4267279c0f457a4ea5668568d
NevilleBAC <neville_bac@outlook.com>
2024-07-03 09:32:59 +0530

    added detail in provider

--- terraform/provider.tf
+++ terraform/provider.tf
@@ -1,11 +1,11 @@
-terraform {
-  required_providers {
-    aws = {
-      source  = "hashicorp/aws"
-      version = "5.54.1"
-    }
-  }
-}
+# terraform {
+#   required_providers {
+#     aws = {
+#       source  = "hashicorp/aws"
+#       version = "5.54.1"
+#     }
+#   }
+# }
 
 provider "aws" {
   region = var.region
