666d4c1a14275ff801d1ea92b6c9a81de6d5368b
NevilleBAC <neville_bac@outlook.com>
2024-07-03 11:41:55 +0530

    added { in ecs.tf

--- terraform/provider.tf
+++ terraform/provider.tf
@@ -1,17 +1,13 @@
-# terraform {
-#   required_providers {
-#     aws = {
-#       source  = "hashicorp/aws"
-#       version = "5.54.1"
-#     }
-#   }
-# }
+terraform {
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "5.54.1"
+    }
+  }
+}
 
 provider "aws" {
   region = var.region
-  assume_role {
-  role_arn = "arn:aws:iam::533266978173:role/aws-service-role/ecs.amazonaws.com/AWSServiceRoleForECS"
-#   execution_role_arn = "arn:aws:iam::533266978173:role/aws-service-role/ecs.amazonaws.com/AWSServiceRoleForECS"
-  }
 }
 
