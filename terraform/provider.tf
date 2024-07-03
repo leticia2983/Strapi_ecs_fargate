fa280aa2fad27ff2df6da1b9a086adef708618a3
NevilleBAC <neville_bac@outlook.com>
2024-07-03 09:56:10 +0530

    added detail in provider

--- terraform/provider.tf
+++ terraform/provider.tf
@@ -10,6 +10,7 @@
 provider "aws" {
   region = var.region
   assume_role {
-  role_arn = arn:aws:iam::533266978173:role/ecs_task_strapi
+    role_arn = "arn:aws:iam::533266978173:role/ecs_task_strapi"
+  }
 }
-}
+
