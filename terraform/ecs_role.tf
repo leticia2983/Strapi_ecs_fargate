aac051f04f04f446cb1bd9e9e1463df285b076a6
NevilleBAC <neville_bac@outlook.com>
2024-07-04 07:26:38 +0530

    added changes to ecs_role

--- terraform/ecs_role.tf
+++ terraform/ecs_role.tf
@@ -1,5 +1,5 @@
 resource "aws_iam_role" "ecs_task_execution_role" {
-  name = "ecs_task_execution_role-strapi"
+  name = "ecs_task_execution_role-strapi-let"
 
   assume_role_policy = jsonencode({
     Version = "2012-10-17",
