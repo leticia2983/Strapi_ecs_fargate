392206fa6632c69ea0a5b8227477700193a2c2e2
NevilleBAC <neville_bac@outlook.com>
2024-07-04 15:17:48 +0530

    added td.json

--- terraform/vpc.tf
+++ terraform/vpc.tf
@@ -1,4 +1,3 @@
-
 resource "aws_vpc" "vpc" {
   cidr_block           = "10.0.0.0/16"
   instance_tenancy     = "default"
@@ -12,23 +11,23 @@ resource "aws_subnet" "sn1" {
   map_public_ip_on_launch = true
 }
 
-
 resource "aws_security_group" "sg" {
   name   = var.security_group
   vpc_id = aws_vpc.vpc.id
 
+  ingress {
+    from_port   = 1337
+    to_port     = 1337
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
 
   ingress {
-      from_port   = 1337
-      to_port     = 1337
-      protocol    = "tcp"
-      cidr_blocks = ["0.0.0.0/0"]
-
-  ingress {
-      from_port   = 3306
-      to_port     = 3306
-      protocol    = "tcp"
-      cidr_blocks = ["0.0.0.0/0"]
+    from_port   = 3306
+    to_port     = 3306
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
 
   ingress {
     description = "https"
@@ -75,6 +74,4 @@ resource "aws_route_table" "rt" {
 resource "aws_route_table_association" "route1" {
   route_table_id = aws_route_table.rt.id
   subnet_id      = aws_subnet.sn1.id
-}
-
 }
