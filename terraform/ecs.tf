ab9bbfc282b8a555b0a8758b95b90848dd3870c6
NevilleBAC <neville_bac@outlook.com>
2024-07-04 07:48:27 +0530

    added changes to ecs_role

--- terraform/ecs.tf
+++ terraform/ecs.tf
@@ -1,14 +1,47 @@
-resource "aws_ecs_cluster" "strapi_cluster" {
-  name = "strapi-cluster"
+resource "aws_ecs_cluster" "ecs" {
+  name = "app_cluster"
 }
 
+resource "aws_ecs_service" "service" {
+  name = "app_service"
+  cluster                = aws_ecs_cluster.ecs.arn
+  launch_type            = "FARGATE"
+  enable_execute_command = true
 
-resource "aws_ecs_task_definition" "strapi" {
-  family                   = "strapi"
-  network_mode             = "awsvpc"
+  deployment_maximum_percent         = 200
+  deployment_minimum_healthy_percent = 100
+  desired_count                      = 1
+  task_definition                    = aws_ecs_task_definition.td.arn
+
+  network_configuration {
+    assign_public_ip = true
+    security_groups  = [aws_security_group.sg.id]
+    subnets          = [aws_subnet.sn1.id, aws_subnet.sn2.id, aws_subnet.sn3.id]
+  }
+}
+
+resource "aws_ecs_task_definition" "td" {
+  container_definitions = jsonencode([
+    {
+      name         = "app"
+      image        = "533266978173.dkr.ecr.us-west-1.amazonaws.com/app_repo"
+      cpu          = 256
+      memory       = 512
+      essential    = true
+      portMappings = [
+        {
+          containerPort = 80
+          hostPort      = 80
+        }
+      ]
+    }
+  ])
+  family                   = "app"
   requires_compatibilities = ["FARGATE"]
-  cpu                      = "1024"
-  memory                   = "2048"
+
+  cpu                = "256"
+  memory             = "512"
+  network_mode       = "awsvpc"
   task_role_arn = "arn:aws:iam::aws:policy/service-role/AmazonECSTaskExecutionRolePolicy"
   execution_role_arn = "arn:aws:iam::aws:policy/service-role/AmazonECSTaskExecutionRolePolicy"
 
