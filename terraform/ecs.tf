1266a27c35e2bf465f9e8ae5271f43afea83dc5b
NevilleBAC <neville_bac@outlook.com>
2024-07-04 07:55:34 +0530

    added changes to ecs_role

--- terraform/ecs.tf
+++ terraform/ecs.tf
@@ -3,10 +3,10 @@ resource "aws_ecs_cluster" "ecs" {
 }
 
 resource "aws_ecs_service" "service" {
-  name = "app_service"
-  cluster                = aws_ecs_cluster.ecs.arn
-  launch_type            = "FARGATE"
-  enable_execute_command = true
+  name                    = "app_service"
+  cluster                 = aws_ecs_cluster.ecs.arn
+  launch_type             = "FARGATE"
+  enable_execute_command  = true
 
   deployment_maximum_percent         = 200
   deployment_minimum_healthy_percent = 100
@@ -20,77 +20,99 @@ resource "aws_ecs_service" "service" {
   }
 }
 
-resource "aws_ecs_task_definition" "td" {
+resource "aws_iam_role" "ecs_task_execution_role" {
+  name = "ecsTaskExecutionRoleStrapi"
 
-  cpu                = "256"
-  memory             = "512"
-  network_mode       = "awsvpc"
-  task_role_arn = "arn:aws:iam::aws:policy/service-role/AmazonECSTaskExecutionRolePolicy"
-  execution_role_arn = "arn:aws:iam::aws:policy/service-role/AmazonECSTaskExecutionRolePolicy"
+  assume_role_policy = jsonencode({
+    Version = "2012-10-17",
+    Statement = [
+      {
+        Effect = "Allow",
+        Principal = {
+          Service = "ecs-tasks.amazonaws.com"
+        },
+        Action = "sts:AssumeRole"
+      }
+    ]
+  })
+}
+
+resource "aws_iam_role_policy_attachment" "ecs_task_execution_role_policy" {
+  role       = aws_iam_role.ecs_task_execution_role.name
+  policy_arn = "arn:aws:iam::aws:policy/service-role/AmazonECSTaskExecutionRolePolicy"
+}
+
+resource "aws_ecs_task_definition" "td" {
+  family               = "strapi-task"
+  cpu                  = "256"
+  memory               = "512"
+  network_mode         = "awsvpc"
+  task_role_arn        = aws_iam_role.ecs_task_execution_role.arn
+  execution_role_arn   = aws_iam_role.ecs_task_execution_role.arn
 
   container_definitions = jsonencode([
     {
-      name      = "strapi-db"
-      image     = "strapi:latest"
-      essential = true
+      name      = "strapi-db",
+      image     = "strapi:latest",
+      essential = true,
       environment = [
         {
-          name  = "mysql"
+          name  = "MYSQL_DATABASE",
           value = "strapi"
         },
         {
-          name  = "strapi_user"
+          name  = "MYSQL_USER",
           value = "strapi_user"
         },
         {
-          name  = "mysql_PASSWORD"
+          name  = "MYSQL_PASSWORD",
           value = "password"
         }
       ],
       portMappings = [
         {
-          containerPort = 5432
+          containerPort = 3306,
           protocol      = "tcp"
         }
       ]
     },
     {
-      name      = "strapi-server"
-      image     = "leticia888444/strapi_docker_final:1.0"
-      essential = true
+      name      = "strapi-server",
+      image     = "leticia888444/strapi_docker_final:1.0",
+      essential = true,
       dependsOn = [{
-        containerName = "strapi"
+        containerName = "strapi-db",
         condition     = "START"
       }],
       environment = [
         {
-          name  = "DATABASE_CLIENT"
+          name  = "DATABASE_CLIENT",
           value = "mysql"
         },
         {
-          name  = "DATABASE_HOST"
+          name  = "DATABASE_HOST",
           value = "localhost"
         },
         {
-          name  = "DATABASE_PORT"
-          value = "5432"
+          name  = "DATABASE_PORT",
+          value = "3306"
         },
         {
-          name  = "DATABASE_NAME"
+          name  = "DATABASE_NAME",
           value = "strapi"
         },
         {
-          name  = "DATABASE_USERNAME"
+          name  = "DATABASE_USERNAME",
           value = "strapi_user"
         },
         {
-          name  = "DATABASE_PASSWORD"
+          name  = "DATABASE_PASSWORD",
           value = "password"
         }
       ],
       portMappings = [
         {
-          containerPort = 1337
+          containerPort = 1337,
           protocol      = "tcp"
         }
       ]
@@ -100,8 +122,8 @@ resource "aws_ecs_task_definition" "td" {
 
 resource "aws_ecs_service" "strapi_service" {
   name            = "strapi-service"
-  cluster         = aws_ecs_cluster.strapi_cluster.id
-  task_definition = aws_ecs_task_definition.strapi.arn
+  cluster         = aws_ecs_cluster.ecs.id
+  task_definition = aws_ecs_task_definition.td.arn
   desired_count   = 1
   launch_type     = "FARGATE"
 
@@ -110,5 +132,4 @@ resource "aws_ecs_service" "strapi_service" {
     security_groups  = [aws_security_group.sg.id]
     assign_public_ip = true
   }
-
 }
